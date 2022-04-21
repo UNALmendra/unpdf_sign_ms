6e2d69c898b665f2e91dacc89b562b79ee2ccf7d
Daniel Arturo Moreno Rincon <damorenor@unal.edu.co>
2022-04-21 06:09:31 -0500

    controller a little bit updated

--- src/controllers/sign.ts
+++ src/controllers/sign.ts
@@ -2,13 +2,13 @@ import { Request, Response, NextFunction } from 'express';
 import axios, { AxiosResponse } from 'axios';
 
 interface Sign {
-    id : number;
+    id_sign : number;
     userId : string;
     image_url : string;
     alt_image : string;
 }
 
-// adding a post
+// adding a signature
 const postSign = async (req: Request, res: Response, next: NextFunction) => {
     // get the data from req.body
     let title: string = req.body.title;
@@ -24,5 +24,20 @@ const postSign = async (req: Request, res: Response, next: NextFunction) => {
     });
 };
 
-export default { postSign };
+const postSign2Pdf = async (req: Request, res: Response, next: NextFunction) => {
+    // get the data from req.body
+    let title: string = req.body.title;
+    let body: string = req.body.body;
+    // add the post
+    let response: AxiosResponse = await axios.post(`https://jsonplaceholder.typicode.com/posts`, {
+        title,
+        body
+    });
+    // return response
+    return res.status(200).json({
+        message: response.data
+    });
+};
+
+export default { postSign, postSign2Pdf };
 
--- src/routes/sign.ts
+++ src/routes/sign.ts
@@ -3,5 +3,6 @@ import controller from '../controllers/sign';
 const router = express.Router();
 
 router.post('/postSign', controller.postSign);
+router.post('/postSign2Pdf', controller.postSign2Pdf);
 
 export = router;
