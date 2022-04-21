b7b8502b7520c453b0f11d303d85c4812c14b278
Daniel Arturo Moreno Rincon <damorenor@unal.edu.co>
2022-04-21 03:56:28 -0500

    docker compose settings fixed

--- postgres/tables/sign.sql
+++ postgres/tables/sign.sql
@@ -1,10 +1,10 @@
 BEGIN TRANSACTION;
 
-CREATE TABLE users (
+CREATE TABLE signs (
     id_sign serial PRIMARY KEY,
     id_user VARCHAR(100), 
     image_url VARCHAR(100),
-    alt_firma VARCHAR(100),
+    alt_firma VARCHAR(100)
 );
 
 COMMIT;
